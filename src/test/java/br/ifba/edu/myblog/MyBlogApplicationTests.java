5b70e46a7ca8f8598c992dc83d869c68e162fb49
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2022-04-07 00:27:20 -0300

    Adicionando projeto

---
+++ src/test/java/br/ifba/edu/myblog/MyBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifba.edu.myblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
