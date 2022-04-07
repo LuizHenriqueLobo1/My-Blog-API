229a8021abb164db7b49d3fdc632175aa39bb53f
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2022-04-07 20:10:26 -0300

    Usando repository no rest controller

--- src/main/java/br/ifba/edu/myblog/controllers/PostController.java
+++ src/main/java/br/ifba/edu/myblog/controllers/PostController.java
@@ -1,17 +1,23 @@
 package br.ifba.edu.myblog.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.ifba.edu.myblog.models.Post;
 
+import br.ifba.edu.myblog.repositories.PostRepository;
+
 @RestController
 public class PostController {
 
+	@Autowired
+	private PostRepository repository;
+	
 	@RequestMapping("/posts")
-	public Post listPost() {
-		Post post = new Post();
-		post.setTitulo("Um título qualquer...");
-		return post;
+	public List<Post> listPost() {
+		return this.repository.findAll();
 	}
 }
