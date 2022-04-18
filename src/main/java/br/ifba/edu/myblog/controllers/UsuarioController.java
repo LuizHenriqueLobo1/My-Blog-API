ab1c4805f799d8cd6144e9f17b86f7860fc7166d
LuizHenriqueLobo1 <luizhenriquewolf@gmail.com>
2022-04-18 17:47:15 -0300

    Adicionando método post para Usuario

--- src/main/java/br/ifba/edu/myblog/controllers/UsuarioController.java
+++ src/main/java/br/ifba/edu/myblog/controllers/UsuarioController.java
@@ -4,25 +4,38 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import br.ifba.edu.myblog.repositories.UsuarioRepository;
 import br.ifba.edu.myblog.dtos.UsuarioDto;
+import br.ifba.edu.myblog.models.Usuario;
 
 @RestController
+@RequestMapping("/usuarios")
 public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository repository;
 
-	@GetMapping("/usuarios")
+	@GetMapping
 	public List<UsuarioDto> listUsuarios() {
 		return UsuarioDto.converte(repository.findAll());
 	}
 
-	@GetMapping("/usuarios/{id}")
+	@GetMapping("/{id}")
 	public UsuarioDto pegarUsuarioByID(@PathVariable long id) {
 		return new UsuarioDto(repository.findById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<Usuario> postarUsuario(@RequestBody Usuario usuario) {
+		repository.save(usuario);
+		return new ResponseEntity<Usuario>(usuario, HttpStatus.CREATED);
 	}
 }
